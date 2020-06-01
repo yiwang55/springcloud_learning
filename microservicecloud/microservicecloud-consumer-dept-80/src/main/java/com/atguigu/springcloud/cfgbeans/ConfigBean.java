09992bc2e53c8e99c2d53652cde903a4811d257c
yiwang <wangyi@ghcchina.com.cn>
2020-06-01 17:28:39 +0800

    spring cloud learning:负载均衡 LB 自定义负载均衡算法 IRule

--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,9 @@
 package com.atguigu.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,4 +17,11 @@ public class ConfigBean {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+//    @Bean
+//    public IRule myRule(){
+////        return new RoundRobinRule();//轮询算法
+////        return new RandomRule();//随机算法
+//        return new RetryRule();//随机重试算法（节点挂掉后尝试几次以后就将其排除）
+//    }
 }
